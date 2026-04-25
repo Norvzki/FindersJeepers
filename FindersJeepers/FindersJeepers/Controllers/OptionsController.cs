ce7a4caf3c415f107c68019f616c6362f82cea25
bob <alexsamcabildo7@gmail.com>
2026-04-25 18:25:30 +0800

    Fix Api route naming

--- FindersJeepers/FindersJeepers/Controllers/OptionsController.cs
+++ FindersJeepers/FindersJeepers/Controllers/OptionsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 
 [ApiController]
-[Route("api/options")]
+[Route("api/v1/options")]
 public class OptionsController : ControllerBase
 {
     private readonly IOptionService _optionService;
@@ -20,7 +20,7 @@ public class OptionsController : ControllerBase
         return Ok(result);
     }
 
-    [HttpGet("drivers/available-for-driver/{driverId:int}")]
+    [HttpGet("jeeps/available-for-driver/{driverId:int}")]
     public async Task<IActionResult> GetJeepsForDriver(int driverId) {
         var result = await _optionService.GetJeepsForDriver(driverId);
         return Ok(result);
