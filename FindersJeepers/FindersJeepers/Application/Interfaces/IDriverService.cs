a62a7a5d0aa73a88de8359cabca8fda68a05f3a4
bob <alexsamcabildo7@gmail.com>
2026-04-25 18:26:03 +0800

    Remove Unnecessary Method in DriverService Interface

--- FindersJeepers/FindersJeepers/Application/Interfaces/IDriverService.cs
+++ FindersJeepers/FindersJeepers/Application/Interfaces/IDriverService.cs
@@ -6,5 +6,4 @@ public interface IDriverService
     Task DeleteAsync(int driverId);
     Task<List<GetDriverResponse>> GetAsync(int pageNumber = -1, int pageSize = -1);
     Task<GetDriverDetailResponse> GetByIdAsync(int driverId);
-    Task<List<DriverSummary>> GetDriverOptionForJeep(int driverId, int jeepId);
 }
