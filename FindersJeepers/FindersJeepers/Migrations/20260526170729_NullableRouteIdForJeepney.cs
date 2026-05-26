6061783a52d43e2894d8b125ebd1ccc383c906f9
bob <alexsamcabildo7@gmail.com>
2026-05-27 02:41:09 +0800

    Migrations for the domain changes (nullable routeId)

---
+++ FindersJeepers/FindersJeepers/Migrations/20260526170729_NullableRouteIdForJeepney.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindersJeepers.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableRouteIdForJeepney : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RouteId",
+                table: "Jeepneys",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RouteId",
+                table: "Jeepneys",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
